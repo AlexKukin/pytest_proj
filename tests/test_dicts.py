7ba5869e24e6b6d2b8a1d46cfe0d33c304ae86bf
Alexander Kukin <KukinAU2008@mail.ru>
2024-02-06 20:02:01 +0400

    Этап 3 (TDD): Рефакторинг тестов. Добалены фикстуры.

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,17 +1,17 @@
+import pytest
 from utils import dicts
 
 
-def test_dicts():
-    test_dict = {"key1": "value1", "key2": "value2"}
+@pytest.mark.parametrize('collection, key, default, expected', [
+    ({"key1": "value1", "key2": "value2"}, "key1", "git", "value1"),
+    ({"key1": "value1", "key2": "value2"}, "key2", "git", "value2"),
+    ({"key1": "value1", "key2": "value2"}, "key2", "git", "value2"),
+    ({"key1": "value1", "key2": "value2"}, "wrong_key", "git", "git"),
+    ({"key1": "value1", "key2": "value2"}, "wrong_key", "my_def_value", "my_def_value"),
+    ({}, "key1", "git", "git"),
+    ({}, "key1", "my_def_value", "my_def_value"),
+    ({"key1": "value1", "key2": "value2"}, "wrong_key", None, None)
+])
+def test_dict(collection, key, default, expected):
+    assert dicts.get_val(collection, key, default) == expected
 
-    assert dicts.get_val(test_dict, "key1") == "value1"
-    assert dicts.get_val(test_dict, "key2") == "value2"
-    assert dicts.get_val(test_dict, "key2", "my_def_value") == "value2"
-
-    assert dicts.get_val(test_dict, "wrong_key") == "git"
-    assert dicts.get_val(test_dict, "wrong_key", "my_def_value") == "my_def_value"
-
-    assert dicts.get_val({}, "key1") == "git"
-    assert dicts.get_val({}, "key1", "my_def_value") == "my_def_value"
-
-    assert dicts.get_val(test_dict, "wrong_key", None) is None
