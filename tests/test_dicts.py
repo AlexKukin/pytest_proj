ef4e3a14954dd4221e72ae160bcfd03d64e836d1
Alexander Kukin <KukinAU2008@mail.ru>
2024-02-06 18:24:23 +0400

    Этап 1 (TDD): Написаны тесты - тесты не проходят

---
+++ tests/test_dicts.py
@@ -0,0 +1,16 @@
+from utils import dicts
+
+
+def test_dicts():
+    test_dict = {"key1": "value1", "key2": "value2"}
+
+    assert dicts.get_val(test_dict, "key1") == "value1"
+    assert dicts.get_val(test_dict, "key2") == "value2"
+    assert dicts.get_val(test_dict, "key2", "my_def_value") == "value2"
+
+    assert dicts.get_val(test_dict, "wrong_key") == "git"
+    assert dicts.get_val(test_dict, "wrong_key", "my_def_value") == "my_def_value"
+
+    assert dicts.get_val([], "key1") == "git"
+    assert dicts.get_val([], "key1", "my_def_value") == "my_def_value"
+
